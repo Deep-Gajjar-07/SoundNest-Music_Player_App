8125dcc5f0f07818b3b5b85fdc922c00751f11da
Deep-Gajjar-07 <gajjardeep2017@gmail.com>
2026-07-25 12:40:03 +0530

    Update library song list item card styling

--- app/src/main/java/com/example/soundnest/ui/screens/libraryscreen/SongListItem.kt
+++ app/src/main/java/com/example/soundnest/ui/screens/libraryscreen/SongListItem.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -23,7 +24,6 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -97,5 +97,7 @@ fun SongListItem(navController: NavController) {
         }
 
     }
+
+    HorizontalDivider(color = Color.DarkGray)
 
 }
