bdc4072574b00d45ed5fdbd4f2a91245cf0c7cb4
Deep-Gajjar-07 <gajjardeep2017@gmail.com>
2026-07-29 12:05:26 +0530

    Add info message to LibraryScreen if song list is empty

--- app/src/main/java/com/example/soundnest/ui/screens/libraryscreen/LibraryScreen.kt
+++ app/src/main/java/com/example/soundnest/ui/screens/libraryscreen/LibraryScreen.kt
@@ -1,18 +1,30 @@
 package com.example.soundnest.ui.screens.libraryscreen
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Inbox
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.example.soundnest.ui.components.AppBottomNavbar
@@ -40,14 +52,44 @@ fun LibraryScreen(navController: NavController) {
 
             Spacer(Modifier.height(12.dp))
 
-            LazyColumn {
+            if (songs.isEmpty()) {
 
-                items(songs) { song ->
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
 
-                    SongListItem(
-                        song = song,
-                        navController = navController
+                    Icon(
+                        imageVector = Icons.Default.Inbox,
+                        contentDescription = null,
+                        tint = Color.Gray,
+                        modifier = Modifier
+                            .size(30.dp)
+                            .padding(bottom = 5.dp)
                     )
+
+                    Text(
+                        text = "Library is Empty!",
+                        color = Color.Gray,
+                        textAlign = TextAlign.Center,
+                        fontWeight = FontWeight.SemiBold,
+                        fontSize = 17.sp,
+                    )
+                }
+
+            } else {
+
+                LazyColumn {
+
+                    items(songs) { song ->
+
+                        SongListItem(
+                            song = song,
+                            navController = navController
+                        )
+
+                    }
 
                 }
 
